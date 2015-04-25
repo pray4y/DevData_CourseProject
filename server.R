3a250ee0dd582cf07acdd31eaf8b59a05dfb5fc8
Ariel <arielzhou.bmc@gmail.com>
2015-04-25 18:12:51 +0800

    add ui.R and server.R

---
+++ server.R
@@ -0,0 +1,199 @@
+op <- function(operand1, operator, operand2) {
+        result <- numeric()
+        sign <- c("+", "-", "*", "/")
+        
+        if (length(operand1) == 1) oper1 <- operand1
+        if (length(operand1) == 2) oper1 <- as.numeric(operand1[1])
+        
+        if (length(operand2) == 1) oper2 <- operand2
+        if (length(operand2) == 2) oper2 <- as.numeric(operand2[1])
+        
+        if (operator == 1) result[1] <- oper1 + oper2
+        if (operator == 2) result[1] <- oper1 - oper2
+        if (operator == 3) result[1] <- oper1 * oper2
+        if (operator == 4) result[1] <- oper1 / oper2
+        
+        if (length(operand1) == 2 && length(operand2) == 1) {
+                result[2] <- paste("(", operand1[2], sign[operator], operand2[1], ")")
+        } else if (length(operand1) == 1 && length(operand2) == 2) {
+                result[2] <- paste("(", operand1[1], sign[operator], operand2[2], ")")
+        } else if (length(operand1) == 2 && length(operand2) == 2) {
+                result[2] <- paste("(", operand1[2], sign[operator], operand2[2], ")")
+        } else if (length(operand1) == 1 && length(operand2) == 1) {
+                result[2] <- paste("(", operand1[1], sign[operator], operand2[1], ")")
+        }
+        
+        return(result)
+}
+
+get24 <- function(card1, card2, card3, card4) {
+        sign <- c("+", "-", "*", "/")
+        where24 <- 0    ## When solution of 24 is found, where24 will equal to some positive number.
+        select24 <- "You cannot get 24 using these four numbers. Please change your selection."
+        card <- c("A = 1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J = 11", "Q = 12", "K = 13", "none")
+
+        int1 <- grep(card1, card)
+        int2 <- grep(card2, card)
+        int3 <- grep(card3, card)
+        int4 <- grep(card4, card)
+#        int1 <- grep(strsplit(card1, " ")[[1]][1], card)
+#        int2 <- grep(strsplit(card2, " ")[[1]][1], card)
+#        int3 <- grep(strsplit(card3, " ")[[1]][1], card)
+#        int4 <- grep(strsplit(card4, " ")[[1]][1], card)
+        
+        if (int1 == 14 | int2 == 14 | int3 == 14 | int4 == 14) {
+                select24 <- "Please select a card from each suit."
+        } else {
+                num <- c(int1, int2, int3, int4)
+                
+                mat_operand <- matrix(data = 00, nrow = 24, ncol = 4)
+                mat_operator <- matrix(data = 0, nrow = 64, ncol = 3)
+                mat_record <- matrix(data = "", nrow = 24 * 64 * 5, ncol = 2)
+                mat_24 <- matrix(nrow = 3, ncol = 2)
+                
+                i <- 1
+                for (n1 in 1:4) {
+                        for (n2 in (1:4)[-n1]) {
+                                for (n3 in (1:4)[-c(n1, n2)]) {
+                                        for (n4 in (1:4)[-c(n1, n2, n3)]) {
+                                                mat_operand[i,] <- c(n1, n2, n3, n4)
+                                                i <- i + 1
+                                        }
+                                }
+                        }
+                }
+                
+                j <- 1
+                for (o1 in 1:4) {
+                        for (o2 in 1:4) {
+                                for (o3 in 1:4) {
+                                        mat_operator[j, ] <- c(o1, o2, o3)
+                                        j <- j + 1
+                                }
+                        }
+                }
+                
+                i <- 1; j <- 1; k <- 1
+                # mat_record, row 1:(24*64), left - center - right
+                for (i in seq_along(1:24)) {
+                        for (j in seq_along(1:64)) {
+                                n1 <- mat_operand[i, 1]; n2 <- mat_operand[i, 2]
+                                n3 <- mat_operand[i, 3]; n4 <- mat_operand[i, 4]
+                                o1 <- mat_operator[j, 1]; o2 <- mat_operator[j, 2]
+                                o3 <- mat_operator[j, 3]
+                                left <- op(num[n1], o1, num[n2]); center <- op(left, o2, num[n3])
+                                mat_record[k,] <- op(center, o3, num[n4])
+                                k <- k + 1
+                        }
+                }
+                
+                where24 <- which(mat_record == "24")
+                if (sum(where24) > 0) {
+                        all24 <- mat_record[where24, 2]
+                        select24 <- paste(all24[1], "= 24")
+                } else {
+                        i <- 1; j <- 1; k <- 24 * 64 + 1
+                        # mat_record, row (24*64+1):(2*24*64), steps: left, right - center
+                        for (i in seq_along(1:24)) {
+                                for (j in seq_along(1:64)) {
+                                        n1 <- mat_operand[i, 1]; n2 <- mat_operand[i, 2]
+                                        n3 <- mat_operand[i, 3]; n4 <- mat_operand[i, 4]
+                                        o1 <- mat_operator[j, 1]; o2 <- mat_operator[j, 2]
+                                        o3 <- mat_operator[j, 3]
+                                        left <- op(num[n1], o1, num[n2]); right <- op(num[n3], o3, num[n4])
+                                        mat_record[k,] <- op(left, o2, right)
+                                        k <- k + 1
+                                }
+                        }
+                        
+                        where24 <- which(mat_record == "24")
+                        if (sum(where24) > 0) {
+                                all24 <- mat_record[where24, 2]
+                                select24 <- paste(all24[1], "= 24")
+                        } else {
+                                i <- 1; j <- 1; k <- 2 * 24 * 64 + 1
+                                # mat_record, row (2*24*64+1):(3*24*64), steps: center - left - right
+                                for (i in seq_along(1:24)) {
+                                        for (j in seq_along(1:64)) {
+                                                n1 <- mat_operand[i, 1]; n2 <- mat_operand[i, 2]
+                                                n3 <- mat_operand[i, 3]; n4 <- mat_operand[i, 4]
+                                                o1 <- mat_operator[j, 1]; o2 <- mat_operator[j, 2]
+                                                o3 <- mat_operator[j, 3]
+                                                center <- op(num[n2], o2, num[n3]); left <- op(num[n1], o1, center)
+                                                mat_record[k,] <- op(left, o3, num[n4])
+                                                k <- k + 1
+                                        }
+                                }
+                                
+                                where24 <- which(mat_record == "24")
+                                if (sum(where24) > 0) {
+                                        all24 <- mat_record[where24, 2]
+                                        select24 <- paste(all24[1], "= 24")
+                                } else {
+                                        i <- 1; j <- 1; k <- 3 * 24 * 64 + 1
+                                        # mat_record, row (3*24*64+1):(4*24*64), steps: center - right - left
+                                        for (i in seq_along(1:24)) {
+                                                for (j in seq_along(1:64)) {
+                                                        n1 <- mat_operand[i, 1]; n2 <- mat_operand[i, 2]
+                                                        n3 <- mat_operand[i, 3]; n4 <- mat_operand[i, 4]
+                                                        o1 <- mat_operator[j, 1]; o2 <- mat_operator[j, 2]
+                                                        o3 <- mat_operator[j, 3]
+                                                        center <- op(num[n2], o2, num[n3]); right <- op(center, o3, num[n4])
+                                                        mat_record[k,] <- op(num[n1], o1, right)
+                                                        k <- k + 1
+                                                }
+                                        }
+                                        
+                                        where24 <- which(mat_record == "24")
+                                        if (sum(where24) > 0) {
+                                                all24 <- mat_record[where24, 2]
+                                                select24 <- paste(all24[1], "= 24")
+                                        } else {
+                                                i <- 1; j <- 1; k <- 4 * 24 * 64 + 1
+                                                # mat_record, row (4*24*64+1):(5*24*64), steps: right - center - left
+                                                for (i in seq_along(1:24)) {
+                                                        for (j in seq_along(1:64)) {
+                                                                n1 <- mat_operand[i, 1]; n2 <- mat_operand[i, 2]
+                                                                n3 <- mat_operand[i, 3]; n4 <- mat_operand[i, 4]
+                                                                o1 <- mat_operator[j, 1]; o2 <- mat_operator[j, 2]
+                                                                o3 <- mat_operator[j, 3]
+                                                                right <- op(num[n3], o3, num[n4]); center <- op(num[n2], o2, right)
+                                                                mat_record[k,] <- op(num[n1], o1, center)
+                                                                k <- k + 1
+                                                        }
+                                                }
+                                                
+                                                where24 <- which(mat_record == "24")
+                                                if (sum(where24) > 0) {
+                                                        all24 <- mat_record[where24, 2]
+                                                        select24 <- paste(all24[1], "= 24")
+                                                }
+                                        }
+                                }
+                        }
+                }
+        }
+        
+        return(select24)
+}
+
+library(shiny)
+shinyServer(
+        function(input, output) {
+                output$t_spade <- renderText({
+                        paste(input$spade)
+                })
+                output$t_heart <- renderText({
+                        paste(input$heart)
+                })
+                output$t_club <- renderText({
+                        paste(input$club)
+                })
+                output$t_diamond <- renderText({
+                        paste(input$diamond)
+                })
+                output$t_main <- renderText({
+                        get24(input$spade, input$heart, input$club, input$diamond)
+                })
+        }
+)
---
+++ ui.R
@@ -0,0 +1,51 @@
+shinyUI(pageWithSidebar(
+        headerPanel('"GET 24!" The Game'),
+        sidebarPanel(
+                selectInput("spade", 
+                        label = "Choose your spade card:",
+                        choices = c("A = 1", "2", "3", "4", "5", "6", "7", "8", 
+                                "9", "10", "J = 11", "Q = 12", "K = 13", "none"),
+                        selected = "none"),
+                selectInput("heart", 
+                        label = "Choose your heart card:",
+                        choices = c("A = 1", "2", "3", "4", "5", "6", "7", "8", 
+                                "9", "10", "J = 11", "Q = 12", "K = 13", "none"),
+                        selected = "none"),
+                selectInput("club", 
+                        label = "Choose your club card:",
+                        choices = c("A = 1", "2", "3", "4", "5", "6", "7", "8", 
+                                "9", "10", "J = 11", "Q = 12", "K = 13", "none"),
+                        selected = "none"),
+                selectInput("diamond", 
+                        label = "Choose your diamond card:",
+                        choices = c("A = 1", "2", "3", "4", "5", "6", "7", "8", 
+                                "9", "10", "J = 11", "Q = 12", "K = 13", "none"),
+                        selected = "none"),
+                p("04-25-2015"),
+                p("by Ariel")
+        ),
+        mainPanel(
+                h3('Your "GET 24!" equation:'),
+                verbatimTextOutput("t_main"),
+                h4("Game Instructions:"),
+                p("Select a card from the dropdown list for each of the four suits 
+                        (spade, heart, club, diamond). 
+                        Every suit has thirteen different cards including", 
+                        code("A"), code("2"), code("3"), code("4"), code("5"), 
+                        code("6"), code("7"), code("8"), code("9"), code("10"), 
+                        code("J"), code("Q"), code("K"), ". 
+                        Here we map A to 1, J to 11, Q to 12, and K to 13, 
+                        so each suit can represent integers from 1 to 13. "),
+                p('After the player selects a card from each suit, "GET 24!" will 
+                        calculate whether the numbers represented by all four 
+                        selected cards can undergo arithmetic operations to give 
+                        a result of 24. Operators allowed in this game are', 
+                        code("+"), code("-"), code("*"), code("/"), '. Pairs of 
+                        brackets are also allowed and are used to clarify 
+                        operator precedence. '),
+                p('If "GET 24!" finds any arithmetic equation which gives 24, 
+                        it will print one complete example above. Otherwise, 
+                        "GET 24!" will ask the player to modify card selection. '),
+                p("Explore and enjoy.")
+        )
+))
